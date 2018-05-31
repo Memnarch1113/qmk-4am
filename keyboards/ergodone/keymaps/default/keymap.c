57630cd811091ca0fea36b615632ed506453a3ff
Gabriel Seltzer <gabeseltzer@gmail.com>
2018-05-31 12:29:52 -0400

    fixed keymap

--- keyboards/ergodone/keymaps/default/keymap.c
+++ keyboards/ergodone/keymaps/default/keymap.c
@@ -4,7 +4,7 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
+#define UPPER 1 // symbols
 #define MDIA 2 // media keys
 
 enum custom_keycodes {
@@ -39,16 +39,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox( \
-  KC_F1,      KC_F2,     KC_GRAVE,     KC_1,       KC_2,          KC_3,          KC_4,         KC_5,
+  KC_ESC,     KC_F2,     KC_GRAVE,     KC_1,       KC_2,          KC_3,          KC_4,         KC_5,
   KC_F3,      KC_F4,     KC_TAB,       KC_Q,       KC_W,          KC_E,          KC_R,         KC_T,
   KC_F5,      KC_F6,     KC_CAPSLOCK,  KC_A,       KC_S,          KC_D,          KC_F,         KC_G,
   KC_F7,      KC_F8,     KC_LSHIFT,    KC_Z,       KC_X,          KC_C,          KC_V,         KC_B,
-  KC_F9,      KC_F10,    KC_LCTRL,     KC_LALT,    KC_LGUI,       KC_SPACE,      KC_NO,        KC_6,
+  KC_F9,      KC_F10,    KC_LCTRL,     KC_LGUI,    KC_LALT,       KC_SPACE,      KC_NO,        KC_6,
 
-  KC_7,      KC_8,       KC_9,         KC_0,       KC_MINUS,      KC_EQUAL,      KC_BSPACE,    KC_HOME,
-  KC_U,      KC_I,       KC_O,         KC_P,       KC_LBRACKET,   KC_RBRACKET,   KC_BSLASH,    KC_PGUP,
-  KC_H,      KC_J,       KC_K,         KC_L,       KC_SCOLON,     KC_QUOTE,      KC_ENTER,     KC_PGDOWN,
-  KC_N,      KC_M,       KC_COMMA,     KC_DOT,     KC_SLASH,      KC_RSHIFT,     KC_UP,        KC_END,
+  KC_7,      KC_8,       KC_9,         KC_0,       KC_MINUS,      KC_EQUAL,      KC_BSPACE,    KC_DELETE,
+  KC_U,      KC_I,       KC_O,         KC_P,       KC_LBRACKET,   KC_RBRACKET,   KC_BSLASH,    KC_HOME,
+  KC_H,      KC_J,       KC_K,         KC_L,       KC_SCOLON,     KC_QUOTE,      KC_ENTER,     KC_PGUP,
+  KC_N,      KC_M,       KC_COMMA,     KC_DOT,     KC_SLASH,      KC_RSHIFT,     KC_UP,        KC_PGDOWN,
+  KC_Y,      KC_SPACE,   KC_RALT,      MO(UPPER),  KC_RCTRL,      KC_LEFT,       KC_DOWN,      KC_RIGHT
+),
+
+[UPPER] = LAYOUT_ergodox( \
+  RESET,    KC_F2,     KC_GRAVE,     KC_1,       KC_2,          KC_3,          KC_4,         KC_5,
+  KC_KP_1,  KC_KP_2,   KC_TAB,       KC_Q,       KC_W,          KC_E,          KC_R,         KC_T,
+  KC_KP_3,  KC_KP_4,   KC_CAPSLOCK,  KC_A,       KC_S,          KC_D,          KC_F,         KC_G,
+  KC_F7,    KC_F8,     KC_LSHIFT,    KC_Z,       KC_X,          KC_C,          KC_V,         KC_B,
+  KC_F9,    KC_F10,    KC_LCTRL,     KC_LALT,    KC_LGUI,       KC_SPACE,      KC_NO,        KC_6,
+
+  KC_7,      KC_8,       KC_9,         KC_0,       KC_MINUS,      KC_EQUAL,      KC_BSPACE,    KC_DELETE,
+  KC_U,      KC_I,       KC_O,         KC_P,       KC_LBRACKET,   KC_RBRACKET,   KC_BSLASH,    KC_HOME,
+  KC_H,      KC_J,       KC_K,         KC_L,       KC_SCOLON,     KC_QUOTE,      KC_ENTER,     KC_PGUP,
+  KC_N,      KC_M,       KC_COMMA,     KC_DOT,     KC_SLASH,      KC_RSHIFT,     KC_UP,        KC_PGDOWN,
   KC_Y,      KC_SPACE,   KC_RALT,      KC_F11,     KC_RCTRL,      KC_LEFT,       KC_DOWN,      KC_RIGHT
 ),
 /* Keymap 1: Symbol Layer
@@ -136,9 +150,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
-};
+// const uint16_t PROGMEM fn_actions[] = {
+//     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+// };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
